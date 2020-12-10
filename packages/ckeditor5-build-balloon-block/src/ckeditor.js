fbdd7889c2bd723197d74a75b2125deb13d0a125
thoarrown <thoarrown@gmail.com>
2020-12-10 14:48:02 +0700

    Update: add css fix UI HTML

--- packages/ckeditor5-build-balloon-block/src/ckeditor.js
+++ packages/ckeditor5-build-balloon-block/src/ckeditor.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable array-bracket-spacing */
 /* eslint-disable space-in-parens */
 /* eslint-disable arrow-parens */
@@ -97,7 +98,8 @@ BalloonEditor.builtinPlugins = [
 	TableProperties,
 	TableCellProperties,
 	HtmlEmbed,
-	Mention
+	Mention,
+	MentionCustomization
 ];
 
 // Editor configuration.
@@ -171,8 +173,8 @@ BalloonEditor.defaultConfig = {
 		feeds: [
 			{
 				marker: '@',
-				feed: ['@Barney', '@Lily', '@Marshall', '@Robin', '@Ted'],
-				minimumCharacters: 1
+				feed: getFeedItems,
+				itemRenderer: customItemRenderer
 			}
 		]
 	},
@@ -198,3 +200,144 @@ BalloonEditor.defaultConfig = {
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
+
+function MentionCustomization(editor) {
+	// The upcast converter will convert <a class="mention" href="" data-user-id="">
+	// elements to the model 'mention' attribute.
+	editor.conversion.for('upcast').elementToAttribute({
+		view: {
+			name: 'a',
+			key: 'data-mention',
+			classes: 'mention',
+			attributes: {
+				href: true,
+				'data-user-id': true
+			}
+		},
+		model: {
+			key: 'mention',
+			value: (viewItem) => {
+				// The mention feature expects that the mention attribute value
+				// in the model is a plain object with a set of additional attributes.
+				// In order to create a proper object, use the toMentionAttribute helper method:
+				const mentionAttribute = editor.plugins
+					.get('Mention')
+					.toMentionAttribute(viewItem, {
+						// Add any other properties that you need.
+						link: viewItem.getAttribute('href'),
+						userId: viewItem.getAttribute('data-user-id')
+					});
+
+				return mentionAttribute;
+			}
+		},
+		converterPriority: 'high'
+	});
+
+	// Downcast the model 'mention' text attribute to a view <a> element.
+	editor.conversion.for('downcast').attributeToElement({
+		model: 'mention',
+		view: (modelAttributeValue, { writer }) => {
+			// Do not convert empty attributes (lack of value means no mention).
+			if (!modelAttributeValue) {
+				return;
+			}
+
+			return writer.createAttributeElement(
+				'a',
+				{
+					class: 'mention',
+					'data-mention': modelAttributeValue.id,
+					'data-user-id': modelAttributeValue.userId,
+					href: modelAttributeValue.link
+				},
+				{
+					// Make mention attribute to be wrapped by other attribute elements.
+					priority: 20,
+					// Prevent merging mentions together.
+					id: modelAttributeValue.uid
+				}
+			);
+		},
+		converterPriority: 'high'
+	});
+}
+
+const items = [
+	{
+		id: '@swarley',
+		userId: '1',
+		name: 'Barney Stinson',
+		link: 'https://www.imdb.com/title/tt0460649/characters/nm0000439'
+	},
+	{
+		id: '@lilypad',
+		userId: '2',
+		name: 'Lily Aldrin',
+		link: 'https://www.imdb.com/title/tt0460649/characters/nm0004989'
+	},
+	{
+		id: '@marshmallow',
+		userId: '3',
+		name: 'Marshall Eriksen',
+		link: 'https://www.imdb.com/title/tt0460649/characters/nm0781981'
+	},
+	{
+		id: '@rsparkles',
+		userId: '4',
+		name: 'Robin Scherbatsky',
+		link: 'https://www.imdb.com/title/tt0460649/characters/nm1130627'
+	},
+	{
+		id: '@tdog',
+		userId: '5',
+		name: 'Ted Mosby',
+		link: 'https://www.imdb.com/title/tt0460649/characters/nm1102140'
+	}
+];
+
+function getFeedItems(queryText) {
+	// As an example of an asynchronous action, return a promise
+	// that resolves after a 100ms timeout.
+	// This can be a server request or any sort of delayed action.
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const itemsToDisplay = items
+				// Filter out the full list of all items to only those matching the query text.
+				.filter(isItemMatching)
+				// Return 10 items max - needed for generic queries when the list may contain hundreds of elements.
+				.slice(0, 10);
+
+			resolve(itemsToDisplay);
+		}, 100);
+	});
+
+	// Filtering function - it uses `name` and `username` properties of an item to find a match.
+	function isItemMatching(item) {
+		// Make the search case-insensitive.
+		const searchString = queryText.toLowerCase();
+
+		// Include an item in the search results if name or username includes the current user input.
+		return (
+			item.name.toLowerCase().includes(searchString) ||
+			item.id.toLowerCase().includes(searchString)
+		);
+	}
+}
+
+function customItemRenderer(item) {
+	const itemElement = document.createElement('span');
+
+	itemElement.classList.add('custom-item');
+	itemElement.id = `mention-list-item-id-${item.userId}`;
+	itemElement.textContent = `${item.name} `;
+
+	const usernameElement = document.createElement('span');
+
+	usernameElement.classList.add('custom-item-username');
+	usernameElement.textContent = item.id;
+
+	itemElement.appendChild(usernameElement);
+
+	return itemElement;
+}
--- packages/ckeditor5-build-balloon-block/src/dist/ckeditor.dev.js
+++ packages/ckeditor5-build-balloon-block/src/dist/ckeditor.dev.js
@@ -121,7 +121,7 @@ function (_BalloonEditorBase) {
 
 
 exports["default"] = BalloonEditor;
-BalloonEditor.builtinPlugins = [_essentials["default"], _uploadadapter["default"], _autoformat["default"], _blocktoolbar["default"], _bold["default"], _italic["default"], _blockquote["default"], _ckfinder["default"], _easyimage["default"], _heading["default"], _image["default"], _imagecaption["default"], _imagestyle["default"], _imagetoolbar["default"], _imageupload["default"], _indent["default"], _link["default"], _list["default"], _mediaembed["default"], _paragraph["default"], _pastefromoffice["default"], _table["default"], _tabletoolbar["default"], _underline["default"], _wordcount["default"], _autosave["default"], _title["default"], _alignment["default"], _texttransformation["default"], _specialcharacters["default"], _specialcharactersarrows["default"], _specialcharacterscurrency["default"], _specialcharactersessentials["default"], _specialcharacterslatin["default"], _specialcharacterstext["default"], _tableproperties["default"], _tablecellproperties["default"], _htmlembed["default"], _mention["default"]]; // Editor configuration.
+BalloonEditor.builtinPlugins = [_essentials["default"], _uploadadapter["default"], _autoformat["default"], _blocktoolbar["default"], _bold["default"], _italic["default"], _blockquote["default"], _ckfinder["default"], _easyimage["default"], _heading["default"], _image["default"], _imagecaption["default"], _imagestyle["default"], _imagetoolbar["default"], _imageupload["default"], _indent["default"], _link["default"], _list["default"], _mediaembed["default"], _paragraph["default"], _pastefromoffice["default"], _table["default"], _tabletoolbar["default"], _underline["default"], _wordcount["default"], _autosave["default"], _title["default"], _alignment["default"], _texttransformation["default"], _specialcharacters["default"], _specialcharactersarrows["default"], _specialcharacterscurrency["default"], _specialcharactersessentials["default"], _specialcharacterslatin["default"], _specialcharacterstext["default"], _tableproperties["default"], _tablecellproperties["default"], _htmlembed["default"], _mention["default"], MentionCustomization]; // Editor configuration.
 
 BalloonEditor.defaultConfig = {
   blockToolbar: ['heading', '|', 'bulletedList', 'numberedList', '|', 'indent', 'outdent', '|', 'imageUpload', 'blockQuote', 'mediaEmbed', '|', 'specialCharacters', 'htmlEmbed'],
@@ -168,8 +168,8 @@ BalloonEditor.defaultConfig = {
   mention: {
     feeds: [{
       marker: '@',
-      feed: ['@Barney', '@Lily', '@Marshall', '@Robin', '@Ted'],
-      minimumCharacters: 1
+      feed: getFeedItems,
+      itemRenderer: customItemRenderer
     }]
   },
   // This value must be kept in sync with the language defined in webpack.config.js.
@@ -191,3 +191,119 @@ BalloonEditor.defaultConfig = {
   // This value must be kept in sync with the language defined in webpack.config.js.
   language: 'en'
 };
+
+function MentionCustomization(editor) {
+  // The upcast converter will convert <a class="mention" href="" data-user-id="">
+  // elements to the model 'mention' attribute.
+  editor.conversion["for"]('upcast').elementToAttribute({
+    view: {
+      name: 'a',
+      key: 'data-mention',
+      classes: 'mention',
+      attributes: {
+        href: true,
+        'data-user-id': true
+      }
+    },
+    model: {
+      key: 'mention',
+      value: function value(viewItem) {
+        // The mention feature expects that the mention attribute value
+        // in the model is a plain object with a set of additional attributes.
+        // In order to create a proper object, use the toMentionAttribute helper method:
+        var mentionAttribute = editor.plugins.get('Mention').toMentionAttribute(viewItem, {
+          // Add any other properties that you need.
+          link: viewItem.getAttribute('href'),
+          userId: viewItem.getAttribute('data-user-id')
+        });
+        return mentionAttribute;
+      }
+    },
+    converterPriority: 'high'
+  }); // Downcast the model 'mention' text attribute to a view <a> element.
+
+  editor.conversion["for"]('downcast').attributeToElement({
+    model: 'mention',
+    view: function view(modelAttributeValue, _ref) {
+      var writer = _ref.writer;
+
+      // Do not convert empty attributes (lack of value means no mention).
+      if (!modelAttributeValue) {
+        return;
+      }
+
+      return writer.createAttributeElement('a', {
+        "class": 'mention',
+        'data-mention': modelAttributeValue.id,
+        'data-user-id': modelAttributeValue.userId,
+        href: modelAttributeValue.link
+      }, {
+        // Make mention attribute to be wrapped by other attribute elements.
+        priority: 20,
+        // Prevent merging mentions together.
+        id: modelAttributeValue.uid
+      });
+    },
+    converterPriority: 'high'
+  });
+}
+
+var items = [{
+  id: '@swarley',
+  userId: '1',
+  name: 'Barney Stinson',
+  link: 'https://www.imdb.com/title/tt0460649/characters/nm0000439'
+}, {
+  id: '@lilypad',
+  userId: '2',
+  name: 'Lily Aldrin',
+  link: 'https://www.imdb.com/title/tt0460649/characters/nm0004989'
+}, {
+  id: '@marshmallow',
+  userId: '3',
+  name: 'Marshall Eriksen',
+  link: 'https://www.imdb.com/title/tt0460649/characters/nm0781981'
+}, {
+  id: '@rsparkles',
+  userId: '4',
+  name: 'Robin Scherbatsky',
+  link: 'https://www.imdb.com/title/tt0460649/characters/nm1130627'
+}, {
+  id: '@tdog',
+  userId: '5',
+  name: 'Ted Mosby',
+  link: 'https://www.imdb.com/title/tt0460649/characters/nm1102140'
+}];
+
+function getFeedItems(queryText) {
+  // As an example of an asynchronous action, return a promise
+  // that resolves after a 100ms timeout.
+  // This can be a server request or any sort of delayed action.
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      var itemsToDisplay = items // Filter out the full list of all items to only those matching the query text.
+      .filter(isItemMatching) // Return 10 items max - needed for generic queries when the list may contain hundreds of elements.
+      .slice(0, 10);
+      resolve(itemsToDisplay);
+    }, 100);
+  }); // Filtering function - it uses `name` and `username` properties of an item to find a match.
+
+  function isItemMatching(item) {
+    // Make the search case-insensitive.
+    var searchString = queryText.toLowerCase(); // Include an item in the search results if name or username includes the current user input.
+
+    return item.name.toLowerCase().includes(searchString) || item.id.toLowerCase().includes(searchString);
+  }
+}
+
+function customItemRenderer(item) {
+  var itemElement = document.createElement('span');
+  itemElement.classList.add('custom-item');
+  itemElement.id = "mention-list-item-id-".concat(item.userId);
+  itemElement.textContent = "".concat(item.name, " ");
+  var usernameElement = document.createElement('span');
+  usernameElement.classList.add('custom-item-username');
+  usernameElement.textContent = item.id;
+  itemElement.appendChild(usernameElement);
+  return itemElement;
+}
